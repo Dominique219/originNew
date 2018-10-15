95ec884a6b151bc42fe059b708e2e2c3d9576112
aihem <aihem@DESKTOP-IUTGI4C.fios-router.home>
2018-10-15 19:32:12 -0400

    First commit Yay~

---
+++ GitPractice/src/ThisIsIt.java
@@ -0,0 +1,16 @@
+
+public class ThisIsIt {
+
+	static int a,b,c;
+	
+	static int Math(int a, int b, int c) {
+		
+		return a*b*c;
+	}
+	public static void main(String[] args) {
+		
+
+		System.out.println(Math(1,2,3));
+		
+	}
+}
